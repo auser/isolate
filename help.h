5776cf0d145bc7060108eff93d4e6cd6f7a93fa0
Ari Lerner <arilerner@mac.com>
2010-01-05 13:57:52 -0800

    Isolate - utility for isolating Unix processes, minimizing their privileges

---
+++ help.h
@@ -0,0 +1,10 @@
+/**
+ * $Id: help.h 12 2010-01-05 11:19:02Z ioerror $
+ */
+
+const string HELP_MESSAGE =
+"Usage: isolate [-hTvMS] [-D path] [-a bytes] [-c bytes] [-d bytes] [-f files]\n"
+"               [-m bytes] [-n bytes] [-p processes] [-r bytes] [-s bytes]\n"
+"               [-t seconds] [-z bytes] program [program-arguments...]\n"
+;
+
